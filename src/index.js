4f5da3110fc5ad7202332831a59578f16f3f0563
amartin58 <adee1001@outlook.com>
2022-03-02 05:27:56 -0800

    Added a no match route

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './routes/Navigation';
 import Home from './routes/Home';
 import Shop from './routes/Shop';
-// import './index.css';
+import './index.css';
 
 render(
   <BrowserRouter>
@@ -11,11 +11,16 @@ render(
       <Route path='/' element={<Navigation />}>
         <Route path='home' element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        {/* <Route path='sign-in' element={<SignIn />} />
-        <Route path='sign-up' element={<SignUp />} /> */}
+    <Route path="*" element={<main>
+      <h1>This page is under construction!</h1>
+    </main>
+    }
+    />
       </Route>
     </Routes>
   </BrowserRouter>,
 
   document.getElementById('root')
 );
+    {/* <Route path='sign-in' element={<SignIn />} />
+        <Route path='sign-up' element={<SignUp />} /> */}
