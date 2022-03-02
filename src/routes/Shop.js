23825fc6cdffcc21abc5e2b8ae1d6eb063dd5303
amartin58 <adee1001@outlook.com>
2022-03-02 13:21:20 -0800

    Update shop styling

--- src/routes/Shop.js
+++ src/routes/Shop.js
@@ -14,115 +14,111 @@ import './Shop.css';
 const Shop = () => {
   return (
     <div>
-      <div>
-        <h2 className='shop-heading__text'>Shop</h2>
-        <div className='featured-collection__container'>
-          <div className='featured-collection__card'>
-            <div>
-              <img src={BlackStudioBag} alt='black backpack' />
+      <h2 className='collection-card__text'>Shop</h2>
+      <main className='collection-card__container'>
+        <div className='collection-card'>
+          <img src={BlackStudioBag} alt='black backpack' />
 
-              <ul>
-                <li>Studio Bag</li>
-                <li>
-                  <span>$15</span>
-                </li>
-              </ul>
-              <button className='featured-btn'>ADD TO CART</button>
-            </div>
-          </div>
+          <ul>
+            <li>Studio Bag</li>
+            <li>
+              <span>$15</span>
+            </li>
+          </ul>
+          <button className='collection-btn'>ADD TO CART</button>
+        </div>
 
-          <div className='featured-collection__card'>
-            <div>
-              <img src={CumulusBag} alt='olive cumulus bag' />
-              <ul>
-                <li>Cumulus</li>
-                <li>
-                  <span>$28</span>
-                </li>
-              </ul>
-              <button className='featured-btn'>ADD TO CART</button>
-            </div>
-          </div>
-
-          <div className='featured-collection__card'>
-            <div>
-              <img src={DanceNylon} alt='dance nylon' />
-              <ul>
-                <li>Dance Bag Nylon</li>
-                <li>
-                  <span>$35</span>
-                </li>
-              </ul>
-              <button className='featured-btn'>ADD TO CART</button>
-            </div>
-          </div>
-
-          <div className='featured-collection__card'>
-            <div>
-              <img src={Stratus} alt='stratus backpack' />
-              <ul>
-                <li>Stratus Backpack</li>
-                <li>
-                  <span>$30</span>
-                </li>
-              </ul>
-              <button className='featured-btn'>ADD TO CART</button>
-            </div>
-          </div>
-
-          <div className='featured-collection__card'>
-            <div>
-              <img src={Cirrus} alt='cirrus' />
-              <ul>
-                <li>Cirrus</li>
-                <li>
-                  <span>$65</span>
-                </li>
-              </ul>
-              <button className='featured-btn'>ADD TO CART</button>
-            </div>
-          </div>
-
-          <div className='featured-collection__card'>
-            <div>
-              <img src={MiniCircle} alt='mini circle' />
-              <ul>
-                <li>Mini Circle</li>
-                <li>
-                  <span>$40</span>
-                </li>
-              </ul>
-              <button className='featured-btn'>ADD TO CART</button>
-            </div>
-          </div>
-
-          <div className='featured-collection__card'>
-            <div>
-              <img src={StudioVaqueta} alt='studio vaqueta' />
-              <ul>
-                <li>Studio Bag Vaqueta</li>
-                <li>
-                  <span>$75</span>
-                </li>
-              </ul>
-              <button className='featured-btn'>ADD TO CART</button>
-            </div>
-          </div>
-
-          <div className='featured-collection__card'>
-            <div>
-              <img src={Sling} alt='sling' />
-              <ul>
-                <li>Sling</li>
-                <li>
-                  <span>$25</span>
-                </li>
-              </ul>
-              <button className='featured-btn'>ADD TO CART</button>
-            </div>
+        <div className='collection-card'>
+          <div>
+            <img src={CumulusBag} alt='olive cumulus bag' />
+            <ul>
+              <li>Cumulus</li>
+              <li>
+                <span>$28</span>
+              </li>
+            </ul>
+            <button className='collection-btn'>ADD TO CART</button>
           </div>
         </div>
-      </div>
+
+        <div className='collection-card'>
+          <div>
+            <img src={DanceNylon} alt='dance nylon' />
+            <ul>
+              <li>Dance Bag Nylon</li>
+              <li>
+                <span>$35</span>
+              </li>
+            </ul>
+            <button className='collection-btn'>ADD TO CART</button>
+          </div>
+        </div>
+
+        <div className='collection-card'>
+          <div>
+            <img src={Stratus} alt='stratus backpack' />
+            <ul>
+              <li>Stratus Backpack</li>
+              <li>
+                <span>$30</span>
+              </li>
+            </ul>
+            <button className='collection-btn'>ADD TO CART</button>
+          </div>
+        </div>
+
+        <div className='collection-card'>
+          <div>
+            <img src={Cirrus} alt='cirrus' />
+            <ul>
+              <li>Cirrus</li>
+              <li>
+                <span>$65</span>
+              </li>
+            </ul>
+            <button className='collection-btn'>ADD TO CART</button>
+          </div>
+        </div>
+
+        <div className='collection-card'>
+          <div>
+            <img src={MiniCircle} alt='mini circle' />
+            <ul>
+              <li>Mini Circle</li>
+              <li>
+                <span>$40</span>
+              </li>
+            </ul>
+            <button className='collection-btn'>ADD TO CART</button>
+          </div>
+        </div>
+
+        <div className='collection-card'>
+          <div>
+            <img src={StudioVaqueta} alt='studio vaqueta' />
+            <ul>
+              <li>Studio Bag Vaqueta</li>
+              <li>
+                <span>$75</span>
+              </li>
+            </ul>
+            <button className='collection-btn'>ADD TO CART</button>
+          </div>
+        </div>
+
+        <div className='collection-card'>
+          <div>
+            <img src={Sling} alt='sling' />
+            <ul>
+              <li>Sling</li>
+              <li>
+                <span>$25</span>
+              </li>
+            </ul>
+            <button className='collection-btn'>ADD TO CART</button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
