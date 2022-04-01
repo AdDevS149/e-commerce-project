1c8744c3f09297a61e23aad5040c1e17759d33ca
amartin58 <adee1001@outlook.com>
2022-04-01 08:45:33 -0700

    Updated deprecated remove to deleteOne

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,14 +1,14 @@
 const asyncHandler = require('express-async-handler');
 
-const Product = require('../models/productModel');
+const Product = require('../models/productModel')
 
 // @desc     Get products
 // @route    GET /api/products
 // @access   Private
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find();
+const products = await Product.find()
 
-  res.status(200).json(products);
+  res.status(200).json(products)
 });
 
 // @desc     set product
@@ -21,21 +21,24 @@ const setProduct = asyncHandler(async (req, res) => {
   }
 
   const product = await Product.create({
-    text: req.body.text,
-  });
+    text: req.body.text
+  })
   res.status(200).json(product);
 });
+
+
 
 // @desc     Update product
 // @route    PUT /api/products/:id
 // @access   Private
 const updateProduct = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  if (!product) {
-    res.status(400);
-    throw new Error('Not a valid product');
-  }
-  const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  const product = await Product.findByIdAndUpdate(req.params.id)
+if(!product) {
+  res.status(400) 
+  throw new Error("Not a valid product")
+}
+const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true
+})
 
   res.status(200).json(updatedProduct);
 });
@@ -44,15 +47,16 @@ const updateProduct = asyncHandler(async (req, res) => {
 // @route    DELETE /api/products/:id
 // @access   Private
 const deleteProduct = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id)
 
-  if (!product) {
-    res.status(400);
-    throw new Error('Product does not exist');
+  if(!product) {
+    res.status(400) 
+    throw new Error("Product does not exist")
   }
-  await Product.remove();
+  await Product.deleteOne()
 
-  res.status(200).json({ id: req.params.id });
+
+  res.status(200).json({ id: req.params.id});
 });
 
 module.exports = {
